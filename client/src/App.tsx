011aa66c4f72582425d55b75b5b33f4509930004
isoto soto <israelss@gmail.com>
2025-10-13 22:33:54 -0400

    fix theme context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,9 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 import Home from "@/pages/home";
-import BlogPage from "@/pages/blog-old";
+import BlogPage from "@/pages/blog";
 import NotFound from "@/pages/not-found";
 
 function Router() {
@@ -21,12 +22,14 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <LanguageProvider>
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </LanguageProvider>
+      <ThemeProvider>
+        <LanguageProvider>
+          <TooltipProvider>
+            <Toaster />
+            <Router />
+          </TooltipProvider>
+        </LanguageProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
