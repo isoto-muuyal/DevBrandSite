3b05c9304e8273cdfb8e75383ea7a79753b4ec82
isoto soto <israelss@gmail.com>
2026-04-02 20:55:27 -0400

    feature: change in validation to upload image

--- client/src/pages/admin.tsx
+++ client/src/pages/admin.tsx
@@ -615,7 +615,7 @@ export default function AdminPage() {
                     <span className="font-medium">Image URL</span>
                     <input
                       className="w-full rounded border border-gray-300 px-3 py-2"
-                      type="url"
+                      type="text"
                       value={draft.imageUrl}
                       onChange={(e) => setDraft({ ...draft, imageUrl: e.target.value })}
                     />
